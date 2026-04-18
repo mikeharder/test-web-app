86047c84b6b309e7cfb827038a2796678157e76f
Mike Harder <mharder@microsoft.com>
2026-04-18 00:19:29 +0000

    fix hostname

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import http from "node:http";
 
-const hostname = "127.0.0.1";
+const hostname = process.env.WEBSITE_HOSTNAME ? "0.0.0.0" : "127.0.0.1";
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
